263cc42902aa12bbc8b2edc82afdc5f9d0105b8b
Mark A. Caprio <mcaprio@nd.edu>
2016-02-27 18:12:34 -0800

    Add module.mk.

---
+++ module.mk
@@ -0,0 +1,28 @@
+$(eval $(begin-module))
+
+################################################################
+# unit definitions
+################################################################
+
+module_units_h := wigner_gsl wigner2_gsl
+module_units_cpp-h := halfint am
+# module_units_f := 
+# module_programs_cpp :=
+# module_programs_f :=
+# module_generated :=
+
+################################################################
+# library creation flag
+################################################################
+
+$(eval $(library))
+
+################################################################
+# special variable assignments, rules, and dependencies
+################################################################
+
+# rule for generating data file
+# $(current-dir)/fort.4 : $(current-dir)/su3genbk
+#	cd $(dir $@); ./$(notdir $<)
+
+$(eval $(end-module))

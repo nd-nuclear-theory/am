de50071313b092aaa8c1ca0cc58e7e81bdbb1798
Patrick Fasano <patrick@patrickfasano.com>
2020-04-10 22:17:23 -0400

    halfint: Provide multiple formatting modes with fmt

--- halfint_fmt.h
+++ halfint_fmt.h
@@ -11,6 +11,7 @@
 
   + 01/10/19 (pjf): Created, based of fmt API documentation.
   + 09/12/19 (pjf): Updated for fmt v6.0.0.
+  + 02/27/20 (pjf): Provide multiple (basic) formatting modes.
 ****************************************************************/
 
 #ifndef HALFINT_FMT_H_
@@ -23,12 +24,39 @@ namespace fmt {
 
 template <>
 struct formatter<HalfInt> {
+  char presentation = 'g';
+
   template <typename ParseContext>
-  constexpr auto parse(ParseContext& ctx) { return ctx.begin(); }
+  constexpr auto parse(ParseContext& ctx) {
+    auto it = ctx.begin(), end = ctx.end();
+    if (it != end && (*it == 'd' || *it == 'f' || *it == 'g')) presentation = *it++;
+
+    // Check if reached the end of the range:
+    if (it != end && *it != '}')
+      throw format_error("invalid format");
+
+    // Return an iterator past the end of the parsed range:
+    return it;
+  }
 
   template <typename FormatContext>
-  auto format(const HalfInt& h, FormatContext& ctx) {
-    return format_to(ctx.out(), "{:s}", h.Str());
+  constexpr auto format(const HalfInt& h, FormatContext& ctx) {
+    if (presentation == 'f')
+    {
+      return format_to(ctx.out(), "{:.1f}", float(h));
+    }
+    else if (presentation == 'd')
+    {
+      if (!h.IsInteger()) throw format_error("non-integer value");
+      return format_to(ctx.out(), "{:d}", int(h));
+    }
+    else
+    {
+      if (h.IsInteger())
+        return format_to(ctx.out(), "{:d}", int(h));
+      else
+        return format_to(ctx.out(), "{:d}/2", h.TwiceValue());
+    }
   }
 };
 
